f74e1c6e469e49dbe8de82602746b9828cb6e904
BugHunter-X <askzenai@gmail.com>
2024-12-29 14:06:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Simulate a long-running task
+  let count = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    count++;
+  }
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
